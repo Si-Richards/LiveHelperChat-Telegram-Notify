483c20ebbc30bab210c69debd0863a2ff2658c8f
Simon <Si-Richards@users.noreply.github.com>
2017-06-13 09:24:53 +0100

    Create bootstrap.php

--- telegram/bootstrap/bootstrap.php
+++ telegram/bootstrap/bootstrap.php
@@ -13,28 +13,24 @@ class erLhcoreClassExtensionTelegram {
 		// Attatch event listeners
 		$dispatcher->listen('chat.chat_started', array($this,'telegram'));
 	}
-		
-	/**
-	 * Arguments
-	 * array('chat' => & $chat)
-	 * */
-	 
+			 
 	public function telegram($params) {
 		
 		$text = "";
-	    $conf = include 'extension/telegram/settings/settings.ini.php';
+		$receiver = "";
+		$veryfyEmail = 	sha1(sha1($receiver.$secretHash).$secretHash);
+	        $conf = include 'extension/telegram/settings/settings.ini.php';
 		$url = 'https://api.telegram.org/bot'.$conf['telegramBot'].'/sendMessage?parse_mode=Markdown';
 		$chat = $params['chat'];
 		
       foreach ($conf['receivers'] as $receiver) {
-
-            $internalurl = $url."&chat_id=xxxxxxxxx";
+                        $internalurl = $url."&chat_id=xxxxxxxxx";
 			$text = "=== NEW CHAT REQUEST ===\nNAME: ";
 			$text = (isset($chat->nick)) ? $text.$chat->nick : $text;
 			$text .= "\nDEPT:".$chat->department;
 			$text .= "\nMESSAGE:".$params['msg']->msg."\n";
 			$text .= "IP:". erLhcoreClassIPDetect::getIP()."\n";
-			$text .= 'https://' . $_SERVER['HTTP_HOST'] . erLhcoreClassDesign::baseurl('chat/accept').'/'.erLhcoreClassModelChatAccept::generateAcceptLink($chat);
+			$text .= 'https://' . $_SERVER['HTTP_HOST'] . erLhcoreClassDesign::baseurl('chat/accept').'/'.erLhcoreClassModelChatAccept::generateAcceptLink($chat).'/'.$veryfyEmail.'/'.$receiver;
 			$internalurl .= "&text=".urlencode($text);
             file_get_contents($internalurl);
         }
